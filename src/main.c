f66d834c83fd7c6b7b1cc725f90d707b9eabb902
hokwach <hezronokwach@gmail.com>
2024-08-12 11:11:08 +0300

    Include database header file Comment the initMenu function for later modification Call initializeDatabase function in the main function

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "header.h"
+#include "database.h" 
 
 void mainMenu(struct User u)
 {
@@ -52,53 +53,53 @@ void mainMenu(struct User u)
     }
 };
 
-void initMenu(struct User *u)
-{
-    int r = 0;
-    int option;
-    system("clear");
-    printf("\n\n\t\t======= ATM =======\n");
-    printf("\n\t\t-->> Feel free to login / register :\n");
-    printf("\n\t\t[1]- login\n");
-    printf("\n\t\t[2]- register\n");
-    printf("\n\t\t[3]- exit\n");
-    while (!r)
-    {
-        scanf("%d", &option);
-        switch (option)
-        {
-        case 1:
-            loginMenu(u->name, u->password);
-            if (strcmp(u->password, getPassword(*u)) == 0)
-            {
-                printf("\n\nPassword Match!");
-            }
-            else
-            {
-                printf("\nWrong password!! or User Name\n");
-                exit(1);
-            }
-            r = 1;
-            break;
-        case 2:
-            // student TODO : add your **Registration** function
-            // here
-            r = 1;
-            break;
-        case 3:
-            exit(1);
-            break;
-        default:
-            printf("Insert a valid operation!\n");
-        }
-    }
-};
+// void initMenu(struct User *u)
+// {
+//     int r = 0;
+//     int option;
+//     system("clear");
+//     printf("\n\n\t\t======= ATM =======\n");
+//     printf("\n\t\t-->> Feel free to login / register :\n");
+//     printf("\n\t\t[1]- login\n");
+//     printf("\n\t\t[2]- register\n");
+//     printf("\n\t\t[3]- exit\n");
+//     while (!r)
+//     {
+//         scanf("%d", &option);
+//         switch (option)
+//         {
+//         case 1:
+//             loginMenu(u->name, u->password);
+//             if (strcmp(u->password, getPassword(*u)) == 0)
+//             {
+//                 printf("\n\nPassword Match!");
+//             }
+//             else
+//             {
+//                 printf("\nWrong password!! or User Name\n");
+//                 exit(1);
+//             }
+//             r = 1;
+//             break;
+//         case 2:
+//             // student TODO : add your **Registration** function
+//             // here
+//             r = 1;
+//             break;
+//         case 3:
+//             exit(1);
+//             break;
+//         default:
+//             printf("Insert a valid operation!\n");
+//         }
+//     }
+// };
 
 int main()
 {
     struct User u;
-    
-    initMenu(&u);
+    sqlite3 *db; // Declare the database pointer
+    // Initialize the database and create tables
+    initializeDatabase(&db);
     mainMenu(u);
-    return 0;
 }
