8b5953021e1d68bd48710f4c9386fa0034a2ec57
hokwach <hezronokwach@gmail.com>
2024-08-16 14:18:59 +0300

    fix: Remove the transfer table Remove unused fields in the users table

--- src/database.c
+++ src/database.c
@@ -3,19 +3,19 @@
 #include "database.h"
 
 // Function to initialize the database
-void initializeDatabase(sqlite3 **db) {
+void initializeDatabase(sqlite3 **db)
+{
     int rc = sqlite3_open("atm_system.db", db);
-    if (rc) {
+    if (rc)
+    {
         fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(*db));
         return;
     }
 
     const char *create_users_table = "CREATE TABLE IF NOT EXISTS users ("
-                                      "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                                      "name TEXT NOT NULL UNIQUE,"
-                                      "password TEXT NOT NULL,"
-                                      "phone TEXT,"
-                                      "country TEXT);";
+                                     "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                                     "name TEXT NOT NULL UNIQUE,"
+                                     "password TEXT NOT NULL);";
 
     const char *create_accounts_table = "CREATE TABLE IF NOT EXISTS accounts ("
                                         "id INTEGER PRIMARY KEY AUTOINCREMENT,"
@@ -29,29 +29,16 @@ void initializeDatabase(sqlite3 **db) {
                                         "country TEXT,"
                                         "FOREIGN KEY (user_id) REFERENCES users(id));";
 
-    const char *create_transfers_table = "CREATE TABLE IF NOT EXISTS transfers ("
-                                         "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                                         "account_number INTEGER,"
-                                         "sender_id INTEGER,"
-                                         "receiver_id INTEGER,"
-                                         "transfer_date TEXT,"
-                                         "FOREIGN KEY (sender_id) REFERENCES users(id),"
-                                         "FOREIGN KEY (receiver_id) REFERENCES users(id),"
-                                         "FOREIGN KEY (account_number) REFERENCES accounts(account_number));";
-
     char *errMsg;
 
-    if (sqlite3_exec(*db, create_users_table, 0, 0, &errMsg) != SQLITE_OK) {
+    if (sqlite3_exec(*db, create_users_table, 0, 0, &errMsg) != SQLITE_OK)
+    {
         fprintf(stderr, "SQL error: %s\n", errMsg);
         sqlite3_free(errMsg);
     }
 
-    if (sqlite3_exec(*db, create_accounts_table, 0, 0, &errMsg) != SQLITE_OK) {
-        fprintf(stderr, "SQL error: %s\n", errMsg);
-        sqlite3_free(errMsg);
-    }
-
-    if (sqlite3_exec(*db, create_transfers_table, 0, 0, &errMsg) != SQLITE_OK) {
+    if (sqlite3_exec(*db, create_accounts_table, 0, 0, &errMsg) != SQLITE_OK)
+    {
         fprintf(stderr, "SQL error: %s\n", errMsg);
         sqlite3_free(errMsg);
     }
