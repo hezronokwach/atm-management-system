e88149a5918408bd8c0197b6746f9fc0b569d297
hezronokwach <hezronokwach@gmail.com>
2024-11-28 11:31:31 +0300

    Add the new initialize database function to the header file

--- src/database.h
+++ src/database.h
@@ -2,8 +2,15 @@
 #define DATABASE_H
 
 #include <sqlite3.h>
-#include "header.h"
 
-void initializeDatabase(sqlite3 **db);
+// Function to initialize the database
+int initializeDatabase(sqlite3 **db);
+
+// Function to check and display user IDs
+void checkUserIds(sqlite3 *db);
+
+// You might want to add these additional functions if you plan to implement them
+int resetUserIdCounter(sqlite3 *db);
+int getMaxUserId(sqlite3 *db);
 
 #endif // DATABASE_H
