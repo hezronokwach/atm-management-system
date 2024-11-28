945e9e34d7281a36c106f8958e0e484af8ec5ca3
hezronokwach <hezronokwach@gmail.com>
2024-11-28 11:32:16 +0300

    Change the option 3 and 4 functions

--- src/main.c
+++ src/main.c
@@ -2,8 +2,9 @@
 #include "database.h"
 #include <stdbool.h>
 
-void mainMenu(struct User u, sqlite3 *db)
+void mainMenu(struct User *u, sqlite3 *db)
 {
+
     int option;
     system("clear");
     printf("\n\n\t\t======= ATM =======\n\n");
@@ -21,40 +22,40 @@ void mainMenu(struct User u, sqlite3 *db)
     switch (option)
     {
     case 1:
-        createNewAcc(u, db);
+        createNewAcc(*u, db);
         break;
     case 2:
-        update(u, db);
+        update(*u, db);
         // student TODO : add your **Update account information** function
         // here
         break;
     case 3:
 
-        int accID;
-        printf("Enter the account ID you want to check: ");
-        scanf("%d", &accID);
-        checkAccountsDetails(accID, db);
+        // int accID;
+        // printf("Enter the account number you want to check: ");
+        // scanf("%d", &accID);
+        checkAccountsDetails(u, db);
         // student TODO : add your **Check the details of existing accounts** function
         // here
         break;
     case 4:
-        checkAllAccounts(u, db);
+        checkAllAccounts(*u, db);
         break;
     case 5:
-    makeTransaction(u,db);
+    makeTransaction(*u,db);
         // student TODO : add your **Make transaction** function
         // here
         break;
     case 6:
-        int accId;
-        printf("Enter the account ID you want to delete: ");
-        scanf("%d", &accId);
-        deleteAccount(accId, db);
+        // int accId;
+        // printf("Enter the account ID you want to delete: ");
+        // scanf("%d", &accId);
+        deleteAccount(u, db);
         // student TODO : add your **Remove existing account** function
         // here
         break;
     case 7:
-    transferAcc(u,db);
+    transferAcc(*u,db);
         // student TODO : add your **Transfer owner** function
         // here
         break;
@@ -104,16 +105,37 @@ void initMenu(struct User *u, sqlite3 *db)
         }
     }
 }
-
 int main()
 {
     struct User u;
     sqlite3 *db; // Declare the database pointer
+    int rc;
 
     // Initialize the database and create tables
-    initializeDatabase(&db);
-    initMenu(&u, db);
+    rc = initializeDatabase(&db);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Failed to initialize database. Error: %s\n", sqlite3_errmsg(db));
+        return 1;
+    }
+
+    // Check current user IDs (for debugging purposes)
+    checkUserIds(db);
+
+    // Main program loop
+    while (1) {
+        initMenu(&u, db);
+        
+        // After each session, ask if the user wants to continue
+        char continue_choice;
+        printf("\nDo you want to perform another operation? (y/n): ");
+        scanf(" %c", &continue_choice);
+        if (continue_choice != 'y' && continue_choice != 'Y') {
+            break;
+        }
+    }
+
     // Close the database connection
     sqlite3_close(db);
+    printf("Thank you for using our ATM system. Goodbye!\n");
     return 0;
 }
